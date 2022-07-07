9cf9a09bf7058626f927bba26aefee9a8ae553bd
Moto <Moto@192.168.0.4>
2022-07-07 20:08:10 -0300

    CRUD corredor completo + carreras sin edicion

--- src/main/java/corr/controlador/AppSevlet.java
+++ src/main/java/corr/controlador/AppSevlet.java
@@ -1,10 +1,14 @@
 package corr.controlador;
 
+import corr.modelo.Carrera;
 import corr.modelo.Modelo;
 import corr.modelo.Corredor;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,19 +32,41 @@ public class AppSevlet extends HttpServlet {
         corredores = new ArrayList();
 
         String accion = request.getParameter("accion");
+        Object accion2 = request.getAttribute("accion2");
         accion = accion == null ? "" : accion;
+        accion = (String) (accion2 != null ? accion2 : accion);
         
         switch (accion) {
+                
+            case "getCarreras":
+                try {
+                    request.setAttribute("cardCorredor", m.getCorredor(Integer.parseInt(request.getParameter("id_corredor"))));
+                } catch (SQLException ex) {
+                    request.setAttribute("cardCorredor", "ERROR: "+ex);
+                } catch (ClassNotFoundException ex) {
+                    request.setAttribute("cardCorredor", "ERROR: "+ex);
+                }
+                
+                try {
+                    request.setAttribute("listCarreras", m.getCarreras(Integer.parseInt(request.getParameter("id_corredor"))));
+                } catch (SQLException ex) {
+                    request.setAttribute("listCarreras", "ERROR: "+ex);
+                } catch (ClassNotFoundException ex) {
+                    request.setAttribute("listCarreras", "ERROR: "+ex);
+                }
+                
+                request.getRequestDispatcher("carreras.jsp").forward(request, response);
+                break;
             
             default: 
                 try {
                     request.setAttribute("cardsCorredores", m.getCorredores());
                 } catch (SQLException ex) {
-                    request.setAttribute("cardsCorredores", "ERROR1");
+                    request.setAttribute("cardsCorredores", "ERROR: "+ex);
                 } catch (ClassNotFoundException ex) {
-                    request.setAttribute("cardsCorredores", "ERROR2");
+                    request.setAttribute("cardsCorredores", "ERROR: "+ex);
                 }
-
+                request.setAttribute("Mensaje", "el mensaje es:"+accion);
                 request.getRequestDispatcher("main.jsp").forward(request, response);
 
                 break;                
@@ -53,7 +79,10 @@ public class AppSevlet extends HttpServlet {
         
             Modelo m = new Modelo();
             Corredor corredor;
+            Carrera carrera;
             int id_corredor;
+            int id_carrera;
+            int id;
             String accion = request.getParameter("accion");
             accion = accion == null ? "" : accion;
             
@@ -97,7 +126,7 @@ public class AppSevlet extends HttpServlet {
                 break;
                 
             case "delCorredor":
-                int id = Integer.parseInt(request.getParameter("id_corredor"));
+                id = Integer.parseInt(request.getParameter("id_corredor"));
                 
                 try {
                     /* Actualizaicón del modelo */
@@ -108,6 +137,42 @@ public class AppSevlet extends HttpServlet {
                     Logger.getLogger(AppSevlet.class.getName()).log(Level.SEVERE, null, ex);
                 }
 
+                doGet(request, response);
+                break;
+                
+            case "newCarrera":
+                carrera = new Carrera();
+                carrera.setTit_carrera(request.getParameter("tit_carrera"));
+                carrera.setFh_carrera(request.getParameter("fh_carrera"));
+                carrera.setKm(Double.parseDouble(request.getParameter("km")));
+                carrera.setMin(Double.parseDouble(request.getParameter("min")));
+                carrera.setId_corredor(Integer.parseInt(request.getParameter("id_corredor")));
+                        
+                try {
+                    m.addCarrera(carrera);
+                } catch (SQLException ex) {
+                    Logger.getLogger(AppSevlet.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(AppSevlet.class.getName()).log(Level.SEVERE, null, ex);
+                }                
+                
+                request.setAttribute("accion2", "getCarreras");
+                doGet(request, response);
+                break;
+
+            case "delCarrera":
+                id = Integer.parseInt(request.getParameter("id_carrera"));
+                
+                try {
+                    /* Actualizaicón del modelo */
+                    m.delCarrera(id);
+                } catch (SQLException ex) {
+                    Logger.getLogger(AppSevlet.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(AppSevlet.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                request.setAttribute("accion2", "getCarreras");
                 doGet(request, response);
                 break;
                 
