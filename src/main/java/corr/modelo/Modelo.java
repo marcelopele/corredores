94794671ed17d18b80b507a035e8f3474cf0034d
Moto <Moto@192.168.0.4>
2022-07-13 09:03:26 -0300

    try autocloseable

--- src/main/java/corr/modelo/Modelo.java
+++ src/main/java/corr/modelo/Modelo.java
@@ -1,56 +1,78 @@
 package corr.modelo;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Modelo {
     Corredor corredor = null;
     Conexion cn = new Conexion();
     PreparedStatement pst;
-    ResultSet rs;    
+    ResultSet rs;
+    
+    private static final String QUERY_getCorredores = "SELECT a.id_corredor, a.nom_corredor, a.ape_corredor, a.img_corredor, Count(b.id_carrera) AS q_carreras, Sum(b.km) AS sumKm, Sum(b.min) AS sumMin, Sum(b.km)/(Sum(b.min)/60) AS vel_promedio FROM corredores AS a LEFT JOIN carreras AS b ON a.id_corredor = b.id_corredor GROUP BY a.id_corredor, a.nom_corredor, a.ape_corredor, a.img_corredor";
+    private static final String QUERY_getCorredor = "SELECT * FROM corredores as a WHERE a.id_corredor = ?";
+    private static final String QUERY_addCorredor = "INSERT INTO `corredores`(`nom_corredor`, `ape_corredor`, `img_corredor`) VALUES (?,?,?)";
+    private static final String QUERY_updCorredor = "UPDATE `corredores` SET `nom_corredor`=?,`ape_corredor`=?,`img_corredor`=? WHERE id_corredor=?";
+    private static final String QUERY_delCorredor = "DELETE FROM `corredores` WHERE id_corredor=?";
+    
+    private static final String QUERY_getCarreras = "SELECT carreras.id_carrera, carreras.tit_carrera, carreras.fh_carrera, carreras.km, carreras.min, carreras.id_corredor FROM carreras WHERE carreras.id_corredor=?";
+    private static final String QUERY_addCarrera = "INSERT INTO `carreras`(`tit_carrera`, `fh_carrera`, `km`, `min`, `id_corredor`) VALUES (?,?,?,?,?)";
+    private static final String QUERY_delCarrera = "DELETE FROM `carreras` WHERE id_carrera=?";
+    private static final String QUERY_updCarrera = "UPDATE `carreras` SET `tit_carrera`=?,`fh_carrera`=?,`km`=?,`min`=?,`id_corredor`=? WHERE id_carrera=?";
+    
     
         public List<Corredor> getCorredores() throws SQLException, ClassNotFoundException {
-            List<Corredor> corredores;
-            corredores = new ArrayList();
             
-            Connection cnn = cn.conectar();
-            pst = cnn.prepareStatement("SELECT a.id_corredor, a.nom_corredor, a.ape_corredor, a.img_corredor, Count(b.id_carrera) AS q_carreras, Sum(b.km) AS sumKm, Sum(b.min) AS sumMin, Sum(b.km)/(Sum(b.min)/60) AS vel_promedio\n" +
-"FROM corredores AS a LEFT JOIN carreras AS b ON a.id_corredor = b.id_corredor\n" +
-"GROUP BY a.id_corredor, a.nom_corredor, a.ape_corredor, a.img_corredor");
-            rs = pst.executeQuery();
+            List<Corredor> corredores = new ArrayList();
             
-            while(rs.next()){
-                int id_corredor=rs.getInt("id_corredor");
-                String nom_corredor=rs.getString("nom_corredor");
-                String ape_corredor=rs.getString("ape_corredor");
-                String img_corredor=rs.getString("img_corredor");
-                int q_carreras=rs.getInt("q_carreras");
-                double vel_corredor_sin_redondear=rs.getDouble("vel_promedio");
-                double vel_corredor=Math.round(vel_corredor_sin_redondear*100.0)/100.0;
+            try(Connection cnn = cn.conectar();
+                PreparedStatement pst = cnn.prepareStatement(QUERY_getCorredores);
+                ResultSet rs = pst.executeQuery()){
+                
+                while(rs.next()){
+                    int id_corredor=rs.getInt("id_corredor");
+                    String nom_corredor=rs.getString("nom_corredor");
+                    String ape_corredor=rs.getString("ape_corredor");
+                    String img_corredor=rs.getString("img_corredor");
+                    int q_carreras=rs.getInt("q_carreras");
+                    double vel_corredor_sin_redondear=rs.getDouble("vel_promedio");
+                    double vel_corredor=Math.round(vel_corredor_sin_redondear*100.0)/100.0;
 
-                corredores.add(new Corredor(id_corredor, nom_corredor, ape_corredor, img_corredor, q_carreras, vel_corredor));
+                    corredores.add(new Corredor(id_corredor, nom_corredor, ape_corredor, img_corredor, q_carreras, vel_corredor));
+                }                
+                
+            }catch(SQLException ex){
+                throw new RuntimeException("Error de SQL",ex);
             }
-            
             return corredores;
         }
         
-        public Corredor getCorredor(int id_corredor) throws SQLException, ClassNotFoundException{
-            Connection cnn = cn.conectar();
-            pst = cnn.prepareStatement("SELECT * FROM corredores as a WHERE a.id_corredor = "+id_corredor);
-            rs = pst.executeQuery();
-            while(rs.next()){
-                id_corredor=rs.getInt("id_corredor");
-                String nom_corredor=rs.getString("nom_corredor");
-                String ape_corredor=rs.getString("ape_corredor");
-                String img_corredor=rs.getString("img_corredor");
+        public Corredor getCorredor(int id_corredor){
+            try(Connection cnn = cn.conectar();
+                PreparedStatement pst = cnn.prepareStatement(QUERY_getCorredor);){
+                
+                pst.setInt(1, id_corredor);
+                try(ResultSet rs = pst.executeQuery();){
+                    while(rs.next()){
+                        id_corredor=rs.getInt("id_corredor");
+                        String nom_corredor=rs.getString("nom_corredor");
+                        String ape_corredor=rs.getString("ape_corredor");
+                        String img_corredor=rs.getString("img_corredor");
 
-                corredor = new Corredor(id_corredor, nom_corredor, ape_corredor, img_corredor);
+                        corredor = new Corredor(id_corredor, nom_corredor, ape_corredor, img_corredor);
+                    }
+                }
+                
+            } catch (SQLException | ClassNotFoundException ex) {
+                throw new RuntimeException("Error de SQL",ex);
             }
+
             return corredor;
         }
         
@@ -61,7 +83,7 @@ public class Modelo {
             String ape_corredor = corredor.getApe_corredor();
             String img_corredor = corredor.getImg_corredor();
 
-            pst = cnn.prepareStatement("INSERT INTO `corredores`(`nom_corredor`, `ape_corredor`, `img_corredor`) VALUES (?,?,?)");
+            pst = cnn.prepareStatement(QUERY_addCorredor);
             pst.setString(1, nom_corredor);
             pst.setString(2, ape_corredor);
             pst.setString(3, img_corredor);
@@ -77,7 +99,12 @@ public class Modelo {
             String ape_corredor = corredor.getApe_corredor();
             String img_corredor = corredor.getImg_corredor();
 
-            pst = cnn.prepareStatement("UPDATE `corredores` SET `nom_corredor`='"+nom_corredor+"',`ape_corredor`='"+ape_corredor+"',`img_corredor`='"+img_corredor+"' WHERE id_corredor="+id_corredor);
+            pst = cnn.prepareStatement(QUERY_updCorredor);
+            pst.setString(1, nom_corredor);
+            pst.setString(2, ape_corredor);
+            pst.setString(3, img_corredor);
+            pst.setInt(4, id_corredor);
+
             pst.executeUpdate();
 
         }
@@ -85,7 +112,8 @@ public class Modelo {
         public void delCorredor(int id_corredor) throws SQLException, ClassNotFoundException{
             Connection cnn = cn.conectar();
 
-            pst = cnn.prepareStatement("DELETE FROM `corredores` WHERE id_corredor="+id_corredor);
+            pst = cnn.prepareStatement(QUERY_delCorredor);
+            pst.setInt(1, id_corredor);
             pst.executeUpdate();
 
         }
@@ -95,7 +123,8 @@ public class Modelo {
             carreras = new ArrayList();
             
             Connection cnn = cn.conectar();
-            pst = cnn.prepareStatement("SELECT carreras.id_carrera, carreras.tit_carrera, carreras.fh_carrera, carreras.km, carreras.min, carreras.id_corredor FROM carreras\n WHERE carreras.id_corredor="+id_corredor);
+            pst = cnn.prepareStatement(QUERY_getCarreras);
+            pst.setInt(1, id_corredor);
             rs = pst.executeQuery();
             
             while(rs.next()){
@@ -120,7 +149,7 @@ public class Modelo {
             double min = carrera.getMin();
             int id_corredor = carrera.getId_corredor();
 
-            pst = cnn.prepareStatement("INSERT INTO `carreras`(`tit_carrera`, `fh_carrera`, `km`, `min`, `id_corredor`) VALUES (?,?,?,?,?)");
+            pst = cnn.prepareStatement(QUERY_addCarrera);
             pst.setString(1, tit_carrera);
             pst.setString(2, fh_carrera);
             pst.setDouble(3, km);
@@ -133,7 +162,8 @@ public class Modelo {
         public void delCarrera(int id_carrera) throws SQLException, ClassNotFoundException{
             Connection cnn = cn.conectar();
 
-            pst = cnn.prepareStatement("DELETE FROM `carreras` WHERE id_carrera="+id_carrera);
+            pst = cnn.prepareStatement(QUERY_delCarrera);
+            pst.setInt(1, id_carrera);
             pst.executeUpdate();
 
         }
@@ -147,7 +177,13 @@ public class Modelo {
             double min = carrera.getMin();
             int id_corredor = carrera.getId_corredor();
 
-            pst = cnn.prepareStatement("UPDATE `carreras` SET `tit_carrera`='"+tit_carrera+"',`fh_carrera`='"+fh_carrera+"',`km`="+km+",`min`="+min+",`id_corredor`="+id_corredor+" WHERE id_carrera="+id_carrera);
+            pst = cnn.prepareStatement(QUERY_updCarrera);
+            pst.setString(1, tit_carrera);
+            pst.setString(2, fh_carrera);
+            pst.setDouble(3, km);
+            pst.setDouble(4, min);
+            pst.setInt(5, id_corredor);
+            pst.setInt(6, id_carrera);
             pst.executeUpdate();
 
         }
